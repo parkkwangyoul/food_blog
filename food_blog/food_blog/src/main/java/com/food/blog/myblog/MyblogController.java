9c36af27a69e5bb272550c275511547f415cdb21
leeseungryoul <zxqw1541@gmail.com>
2013-12-02 17:29:44 +0900

    세션처리 수정.

--- food_blog/food_blog/src/main/java/com/food/blog/myblog/MyblogController.java
+++ food_blog/food_blog/src/main/java/com/food/blog/myblog/MyblogController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,18 +27,28 @@ public class MyblogController {
 
 		return mav;
 	}
-	
 	@RequestMapping(value = "/blog", method = RequestMethod.POST)
-	public ModelAndView goBlog(HttpSession session, HttpServletRequest request){
+	public ModelAndView goBlog( HttpServletRequest request, UserInfo userInfo){
 		ModelAndView mav = new ModelAndView();
 		
-		UserInfo userInfo = getSession(session);
 		System.out.println(userInfo);
 		
 
 		mav.setViewName("blog/blog");
 		return mav;
 	}
+	
+	/*
+	@RequestMapping(value = "/blog/{userId}", method = RequestMethod.POST)
+	public ModelAndView goBlog( HttpServletRequest request, UserInfo userInfo, @PathVariable String userId){
+		ModelAndView mav = new ModelAndView();
+		
+		System.out.println(userInfo);
+		
+
+		mav.setViewName("blog/blog");
+		return mav;
+	}*/
 	private UserInfo getSession(HttpSession session) {
 		UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
 		
